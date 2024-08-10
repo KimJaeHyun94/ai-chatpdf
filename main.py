c93b3e22bb3fc76ba4fc326c4511b3419f47bad7
KimJaeHyun94 <48156989+KimJaeHyun94@users.noreply.github.com>
2024-08-10 13:10:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ import os
 st.title("ChatPDF")
 st.write("---")
 
+#OpenAI KEY 입력 받기
+openai_key = st.text_input('OPEN_AI_API_KEY', type="password")
+
 # 파일 업로드
 uploaded_file = st.file_uploader("PDF 파일을 올려주세요!", type=['pdf'])
 st.write("---")
@@ -42,7 +45,7 @@ if uploaded_file is not None:
     texts = text_splitter.split_documents(pages)
 
     #Embedding
-    embeddings_model = OpenAIEmbeddings()
+    embeddings_model = OpenAIEmbeddings(openai_api_key=openai_key)
 
     # Chroma 데이터베이스에 저장
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -54,7 +57,7 @@ if uploaded_file is not None:
 
         if st.button('질문하기'):
             with st.spinner('잠시만 기다려 주세요...'):
-                llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+                llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0,  openai_api_key=openai_key, streaming=True, callbacks=[stream_hander])
                 qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
                 result = qa_chain({"query": question})
                 st.write(result["result"])
